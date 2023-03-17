481c605b2a0fcfd59575dac84b94ffc51beed49f
Mendo6472 <mendo6472@gmail.com>
2023-03-17 10:48:28 -0500

    Splitting test into two classes

--- Seguimiento-7/test/model/RecipeTest.java
+++ Seguimiento-7/test/model/RecipeTest.java
@@ -1,24 +1,14 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class Testing {
-    
-    Ingredient setupStage1Ingredient;
+public class RecipeTest {
     Recipe setupStage1Recipe;
     Recipe setupStage2Recipe;
 
     public void setupStage1(){
-        try {
-            setupStage1Ingredient = new Ingredient();
-            setupStage1Ingredient.setName("Tomate");
-            setupStage1Ingredient.setWeight(245);
-            setupStage1Recipe = new Recipe();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        setupStage1Recipe = new Recipe();
     }
 
     public void setupStage2(){
@@ -30,65 +20,6 @@ class Testing {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-    }
-
-    @Test
-    public void addWeightToIngredient(){
-        setupStage1();
-        boolean weightChanged = false;
-        int weightToAdd = 54;
-        try {
-            setupStage1Ingredient.addWeight(weightToAdd);
-            weightChanged = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        assertTrue(weightChanged);//Se verifica que el boolean adentro del try haya cambiado a true
-        assertEquals(299, setupStage1Ingredient.getWeight());//Se vefirica que el ingrediente tenga el peso correcto
-    }
-
-    @Test
-    public void addWrongWeightToIngredient(){
-        setupStage1();
-        boolean weightChanged = false;
-        int weightToAdd = -100;
-        try {
-            setupStage1Ingredient.addWeight(weightToAdd);
-            weightChanged = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        assertFalse(weightChanged);//Se verifica que el boolean no haya cambiado a true como pasa dentro del try
-    }
-
-    @Test
-    public void removeWeightFromIngredient(){
-        setupStage1();
-        boolean weightChanged = false;
-        int weightToRemove = 45;
-        try {
-            setupStage1Ingredient.removeWeight(weightToRemove);
-            weightChanged = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        assertTrue(weightChanged);//Se verifica que el boolean adentro del try haya cambiado a true
-        assertEquals(200, setupStage1Ingredient.getWeight());//Se vefirica que el ingrediente tenga el peso correcto
-    }
-
-    @Test
-    public void removeWrongWeightFromIngredient(){
-        setupStage1();
-        boolean weightChanged = false;
-        int weightToRemove = -100;
-        try {
-            setupStage1Ingredient.removeWeight(weightToRemove);
-            weightChanged = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        assertFalse(weightChanged);//Se verifica que el boolean no haya cambiado a true como pasa dentro del try
     }
 
     @Test
@@ -174,5 +105,7 @@ class Testing {
         assertNotNull(setupStage2Recipe.searchIngredientNameOnRecipe("Arroz"));
         assertEquals(2, setupStage2Recipe.countIngredients());//Se verifica que hayan 2 ingredientes en la receta
     }
+
+
 
 }
